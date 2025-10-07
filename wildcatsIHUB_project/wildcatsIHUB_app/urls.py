91a98dedac54f97ac0ed8e16d5e6812d2722e66e
seobi <sophie.aloria@gmail.com>
2025-10-08 01:37:17 +0800

    finishing userProfile

--- wildcatsIHUB_project/wildcatsIHUB_app/urls.py
+++ wildcatsIHUB_project/wildcatsIHUB_app/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('submit/', views.submit_project, name='submit_project'),
     path("dashboard/", views.dashboard, name='dashboard'),
     path('user-profile/', views.user_profile, name='user_profile'),
+    path('projects/', views.view_project, name='view_project'),
+    path('project/<int:project_id>/', views.view_project, name='view_project'),
+    path('delete_project/<int:project_id>/', views.delete_project, name='delete_project'),
+
+
 ]
