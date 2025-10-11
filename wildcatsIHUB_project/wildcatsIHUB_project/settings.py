b2771b52b550db164c98b3483451d682ed760e94
seobi <sophie.aloria@gmail.com>
2025-10-11 21:44:59 +0800

    Configured Supabase connection in settings.py and added .env

--- wildcatsIHUB_project/wildcatsIHUB_project/settings.py
+++ wildcatsIHUB_project/wildcatsIHUB_project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env 
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+         conn_max_age=600,    # persistent connections
+         ssl_require=True     # enforce SSL
     }
 }
 
