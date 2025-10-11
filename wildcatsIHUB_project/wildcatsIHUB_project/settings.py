415273d954db04a95e8317735a106b8e14aef511
seobi <sophie.aloria@gmail.com>
2025-10-11 22:58:09 +0800

    connect to supabase

--- wildcatsIHUB_project/wildcatsIHUB_project/settings.py
+++ wildcatsIHUB_project/wildcatsIHUB_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 
@@ -79,14 +80,15 @@ WSGI_APPLICATION = 'wildcatsIHUB_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-         conn_max_age=600,    # persistent connections
-         ssl_require=True     # enforce SSL
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,    # persistent connections
+        ssl_require=True     # enforce SSL
+    )
 }
+
 
 
 # Password validation
