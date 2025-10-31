69193dfebab425e4b428a8b3d34cf30fa42620b4
seobi <sophie.aloria@gmail.com>
2025-11-01 01:39:26 +0800

    added reset password

--- wildcatsIHUB_project/accounts/urls.py
+++ wildcatsIHUB_project/accounts/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
-    path('forgot-password/', views.forgot_password, name='forgot_password'),
     path('resend-verification/', views.resend_verification, name='resend_verification'),
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('reset-password/', views.reset_password_confirm, name='reset_password_confirm'),  
+    path('reset-password-done/', views.reset_password_done, name='reset_password_done'),
 ]
